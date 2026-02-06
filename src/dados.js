426ce7feaede19842d15d74ded0f324e056296cd
𝐄𝐦𝐞𝐫𝐬𝐨𝐧 𝐏𝐞𝐬𝐬𝐨𝐚 <60630092+emersonpessoa01@users.noreply.github.com>
2026-02-06 20:34:07 -0300

    feat: Criado rota growdevers :rocket:

---
+++ src/dados.js
@@ -0,0 +1,16 @@
+export const growdevers = [
+  {
+    id: 1,
+    nome: "Maria",
+    email: "maria@org.com",
+    idade: 25,
+    matriculado: true,
+  },
+  {
+    id: 2,
+    nome: "João",
+    email: "joao@org.com",
+    idade: 30,
+    matriculado: false,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
-// Crie um api rest começando com get
-// src/app.js
 import express from "express";
 import * as dotenv from "dotenv";
+import { growdevers } from "./dados.js";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 /* Criar nossas rotas */
-app.get("/hello", (req, res) => {
-  res.json({
-    message: "Olá, esta é a API REST!",
-  });
-});
-
-app.get("/about", (req, res) => {
-  res.json({
-    nome: "Emerson Pessoa",
-    email: "emersonpessoa@growdev.com.br",
-    resumo: "Aluno da Growdev",
-    idade: 52,
-    skills: ["JavaScript", "Node.js", "Express", "React"],
+/* GET /growdevers - listar growdevers */
+app.get("/growdevers", (req, res) => {
+  res.status(200).json({
+    ok: true,
+    mensagem: "Lista de growdevers",
+    growdevers,
   });
 });
 
