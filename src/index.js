627059ab69982be2b6eb9ea008ea5e63a3b413a0
𝐄𝐦𝐞𝐫𝐬𝐨𝐧 𝐏𝐞𝐬𝐬𝐨𝐚 <60630092+emersonpessoa01@users.noreply.github.com>
2026-02-06 20:13:14 -0300

    feat: Realizado exercício com verbo GET :sparkles:

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 // Crie um api rest começando com get
 // src/app.js
 import express from "express";
-const app = express();
-export default app;
+import * as dotenv from "dotenv";
+dotenv.config();
 
+const app = express();
 app.use(express.json());
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Olá, esta é a API REST!" });
+/* Criar nossas rotas */
+app.get("/hello", (req, res) => {
+  res.json({
+    message: "Olá, esta é a API REST!",
+  });
 });
 
-const PORT = 3000;
+app.get("/about", (req, res) => {
+  res.json({
+    nome: "Emerson Pessoa",
+    email: "emersonpessoa@growdev.com.br",
+    resumo: "Aluno da Growdev",
+    idade: 52,
+    skills: ["JavaScript", "Node.js", "Express", "React"],
+  });
+});
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT} e http://localhost:${PORT}/api`);
+  console.log(`Servidor rodando na porta ${PORT} e http://localhost:${PORT}`);
 });
